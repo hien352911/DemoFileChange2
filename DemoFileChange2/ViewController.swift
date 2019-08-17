b2b6ac824ee1815950f08b1d00f602c2edc0f0ed
hien.bui <yuusaku.kd352911@gmail.com>
2019-08-17 12:00:02 +0700

    init commit

---
+++ DemoFileChange2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoFileChange2
+//
+//  Created by MTQ on 8/17/19.
+//  Copyright © 2019 MTQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'DemoFileChange2' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for DemoFileChange2
+pod 'Device', '3.1.0'
+end
